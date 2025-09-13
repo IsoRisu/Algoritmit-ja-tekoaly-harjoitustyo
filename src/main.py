739aa7abb5d0fe3ed45fbdcc53e0053f58f28fad
Risu <kraastian2@gmail.com>
2025-09-13 17:20:59 +0300

    Testing AI platform

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import random
+
 def main():
     while True:
         # Read input commands from the program
@@ -5,6 +7,10 @@ def main():
         
         # Process input commands and generate output responses
         # Implement your AI logic here
+
+        move = random.randint(0,6)
+        print(f"MOVE:{move}")
+        print("jipii")
         
 if __name__ == "__main__":
     main()
